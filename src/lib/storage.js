2e5cffedb88f133b044c0bbda2c56984c864f644
sunnabj <sunnazool@gmail.com>
2018-11-19 16:44:56 +0000

    leikur spilast

---
+++ src/lib/storage.js
@@ -0,0 +1,35 @@
+/**
+ * Sækir og vistar í localStorage
+ */
+
+// Fast sem skilgreinir heiti á lykli sem vistað er undir í localStorage
+const LOCALSTORAGE_KEY = 'calc_game_scores';
+
+/**
+ * Sækir gögn úr localStorage. Skilað sem röðuðum lista á forminu:
+ * { points: <stig>, name: <nafn> }
+ *
+ * @returns {array} Raðað fylki af svörum eða tóma fylkið ef ekkert vistað.
+ */
+export function load() {
+  // todo útfæra
+  // Kalla á þetta þegar leikurinn byrjar. Ef eitthvað vistað þá bætum við því við í html-ið.
+}
+
+/**
+ * Vista stig
+ *
+ * @param {string} name Nafn þess sem á að vista
+ * @param {number} points Stig sem á að vista
+ */
+export function save(name, points) {
+  // todo útfæra
+  // Vistar inn í local storage
+}
+
+/**
+ * Hreinsa öll stig úr localStorage
+ */
+export function clear() {
+  // todo útfæra
+}
